0fa76a4c2fbe7ea60148baaf9f4542cb4ef90a62
doodleblob <charles-barry@outlook.com>
2022-12-08 21:39:59 +0000

    Routing setup

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-const Koa = require('koa');
+'use strict';
 
-const defaultPort = 8080
+const Koa = require('koa');
+const Router = require('@koa/router');
+
+const defaultPort = 1337
 const port = process.env.PORT || defaultPort
 
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch(err) {
+    console.log(err.status)
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
 });
 
+router.get('/', (ctx) => {
+  ctx.body = 'Main';
+});
 
+router.get('/projects', (ctx) => {
+  ctx.body = 'Projects'
+});
 
+router.get('/projects/:project_id', (ctx) => {
+  ctx.body = 'Projects ID: ' + ctx.params.project_id
+});
 
+router.get('/blog', (ctx) => {
+  ctx.body = 'Blog'
+})
+
+router.get('/blog/:blog_id', (ctx) => {
+  ctx.body = 'Blog ID: ' + ctx.params.blog_id
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods());
 
 module.exports = app.listen(port, () => console.log(`Listening on port: ${port}`))
