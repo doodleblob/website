6f084036635ad0640363037f44640e4c5263c312
doodleblob <charles-barry@outlook.com>
2022-12-08 22:59:27 +0000

    Add initial pages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const Koa = require('koa');
 const Router = require('@koa/router');
+const render = require('koa-ejs');
+const path = require('path');
 
 const defaultPort = 1337
 const port = process.env.PORT || defaultPort
@@ -9,17 +11,25 @@ const port = process.env.PORT || defaultPort
 const app = new Koa();
 const router = new Router();
 
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: false,
+  viewExt: 'html',
+  cache: false
+});
+
 app.use(async (ctx, next) => {
   try {
     await next()
+    if (ctx.status === 404) ctx.throw(404)
   } catch(err) {
     console.log(err.status)
     ctx.status = err.status || 500;
-    ctx.body = err.message;
+    ctx.body = `Error ${err.status}\n${err.message}`;
   }
 });
 
-router.get('/', (ctx) => {
+router.get('/', (ctx, next) => {
   ctx.body = 'Main';
 });
 
